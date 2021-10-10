aa4f41bc7450631fb099e3314646ec6a071b0abe
Keenah <kvancamp@mail.com>
2021-10-10 03:03:21 -0500

    commit tree df9446694e8616d6b1064c9c40a113360b2093e7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    validates :title, presence:true, allow_blank: false
+    validates :content, length: { minimum: 100,
+    too_short: "%{count} characters is the minimum allowed" }, allow_blank: false
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+    message: "%{value} is not a valid category" }
 end
